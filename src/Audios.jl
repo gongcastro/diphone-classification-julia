b51787e739a2b95d3d8de71244012dc0871bf08b
Gonzalo García-Castro <31894043+gongcastro@users.noreply.github.com>
2024-04-12 17:01:44 +0200

    Some spectograms had NaNs after transformation, now replaced with random numbers [0, 1]

--- src/Audios.jl
+++ src/Audios.jl
@@ -5,63 +5,66 @@ using StatsBase
 using ProgressMeter
 
 export import_audios,
-	make_spectrograms,
-	pad_spectrograms
+    make_spectrograms,
+    pad_spectrograms
 
 # read WAV files into an array
 function import_audios(files::Vector{String})
-	signal = []
-	sr = []
-	@showprogress "Importing audios..." for f ∈ files
-		y, s = wavread(f)
-		push!(signal, vec(y))
-		push!(sr, s)
-	end
-	return signal, sr
+    signal = []
+    sr = []
+    @showprogress "Importing audios..." for f ∈ files
+        y, s = wavread(f)
+        push!(signal, vec(y))
+        push!(sr, s)
+    end
+    return signal, sr
 end
 
 # spectrograms
 mutable struct Spectrogram
-	time::Vector{Float64}
-	frequency::Vector{Float64}
-	db::Matrix{Float64}
+    time::Vector{Float64}
+    frequency::Vector{Float64}
+    db::Matrix{Float64}
 
-	function Spectrogram(signal::Vector{Float64})
-		spec = spectrogram(signal, 509)
-		return new(spec.time, log.(spec.freq), pow2db.(spec.power))
-	end
+    function Spectrogram(signal::Vector{Float64})
+        spec = spectrogram(signal, 509)
+        return new(spec.time, log.(spec.freq), pow2db.(spec.power))
+    end
 end
 
+replace_nan(v) = map(x -> isnan(x) ? rand(Float32, 1)[1] : x, v)
+
 # input data
-function make_spectrograms(x::Vector; trans = nothing)
-	@showprogress "Generating spectrograms..." for (i, xi) ∈ enumerate(x)
-		xi = transpose(Spectrogram(xi).db)
-		if trans !== nothing
-			dt = fit(trans, xi, dims = 2)
-			xi = StatsBase.transform(dt, xi)
-		end
-		x[i] = xi
-	end
-	return x
+function make_spectrograms(x::Vector; trans=nothing)
+    @showprogress "Generating spectrograms..." for (i, xi) ∈ enumerate(x)
+        xi = permutedims(Spectrogram(xi).db)
+        if trans !== nothing
+            dt = fit(trans, xi, dims=2)
+            xi = StatsBase.transform(dt, xi)
+            xi = replace_nan(xi)
+        end
+        x[i] = xi
+    end
+    return x
 end
 
 # pad zeroes to make all matrices of same size
 function pad_spectrograms(x::Vector, cols::Int)
-	@showprogress "Padding spectograms to $(cols) time steps..." for (i, xi) ∈ enumerate(x)
-		if size(xi, 1) < cols
-			z = rand(Float64, (cols - size(xi, 1), size(xi, 2)))
-			xi = vcat(xi, z)
-		elseif size(xi, 1) > cols
-			@warn "Element $(i) more than $(cols) timestamps"
-		end
-		x[i] = xi
-	end
-	return x
-	@info "Padded spectograms to be $(cols) time steps long"
+    @showprogress "Padding spectograms to $(cols) time steps..." for (i, xi) ∈ enumerate(x)
+        if size(xi, 1) < cols
+            z = rand(Float64, (cols - size(xi, 1), size(xi, 2)))
+            xi = vcat(xi, z)
+        elseif size(xi, 1) > cols
+            @warn "Element $(i) more than $(cols) timestamps"
+        end
+        x[i] = xi
+    end
+    return x
+    @info "Padded spectograms to be $(cols) time steps long"
 end
 
-function plot_spectrogram(x::Spectrogram; file::String = nothing)
-	heatmap(x.db)
+function plot_spectrogram(x::Spectrogram; file::String=nothing)
+    heatmap(x.db)
 end
 
 end
--- src/Model.jl
+++ src/Model.jl
@@ -2,57 +2,57 @@ module Model
 
 include("./Audios.jl")
 using .Audios
-using Flux, CUDA, Statistics, StatsBase, OneHotArrays
+using Flux, Statistics, StatsBase, OneHotArrays
 using Flux.Losses: mse
 using ProgressMeter
 
 export make_features,
-	make_targets,
-	Args,
-	get_loss,
-	get_accuracy,
-	make_predictions,
-	train_model
+    make_targets,
+    Args,
+    get_loss,
+    get_accuracy,
+    make_predictions,
+    train_model
 
-function make_features(files::Vector{String}; pad::Bool = true, trans = nothing)
-	audios, sr = import_audios(files)
-	x = make_spectrograms(audios; trans = trans)
-	max_cols = maximum(map(x -> size(x, 1), x))
-	if pad
-		x = pad_spectrograms(x, max_cols)
-	end
-	x = convert.(Array{Float32}, x)
-	return transpose.(x)
+function make_features(files::Vector{String}; pad::Bool=true, trans=nothing)
+    audios, sr = import_audios(files)
+    x = make_spectrograms(audios; trans=trans)
+    max_cols = maximum(map(x -> size(x, 1), x))
+    if pad
+        x = pad_spectrograms(x, max_cols)
+    end
+    x = convert.(Array{Float32}, x)
+    return permutedims.(x)
 end
 
 function make_targets(files::Vector{String})
-	labels = getindex.(split.(basename.(files), "_"), 1)
-	targets = convert.(Int32, labels .== "CV")
-	return targets, labels
+    labels = getindex.(split.(basename.(files), "_"), 1)
+    targets = convert.(Int32, labels .== "CV")
+    return targets, labels
 end
 
 
 Base.@kwdef mutable struct Args
-	batch_size::Int = 64 # batch size
-	η::Float64 = 1e-3 # learning rate
-	epochs::Int = 1e3 # number of epochs
-	throttle::Int = 10
+    batch_size::Int = 64 # batch size
+    η::Float64 = 1e-3 # learning rate
+    epochs::Int = 1e3 # number of epochs
+    throttle::Int = 10
 end
 
 
 function get_accuracy(result, label)
-	pred = convert.(Int, result .>= 0.5)
-	acc = mean(pred .== label)
-	return acc
+    pred = convert.(Int, result .>= 0.5)
+    acc = mean(pred .== label)
+    return acc
 end
 
 function make_predictions(model, x, y)
-	probs = [model(i) for i in x]
-	preds = [p .>= 0.5 for p in probs]
-	last_pred = [x[end] for (x) in preds]
-	acc = mean(last_pred .== y)
+    probs = [model(i) for i in x]
+    preds = [p .>= 0.5 for p in probs]
+    last_pred = [x[end] for (x) in preds]
+    acc = mean(last_pred .== y)
 
-	return probs, preds, acc
+    return probs, preds, acc
 end
 
 end
